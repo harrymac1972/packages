519bb126eb611b6529a08121f8b47ba7fdd0614b
harry <harrymac1972@gmail.com>
2024-02-02 18:50:42 -0700

    init

---
+++ BigBox/Box/Data/Form.py
@@ -0,0 +1,24 @@
+
+
+class CSV:
+
+    def append(self):
+        print("append")
+
+class Invoice:
+
+    def create(self):
+        print("create")
+
+    def read(self):
+        print("\n\tread")
+        print("\n\tI'm a method")
+        print("\n\tin a class")
+        print("\n\tin a subPackage")
+        print("\n\tin a Package")
+
+    def update(self):
+        print("update")
+
+    def delete(self):
+        print("delete")
---
+++ _main.py
@@ -0,0 +1,9 @@
+
+#    P   sP   M           C
+from BigBox.Box.Data.Form import Invoice
+
+
+
+inv = Invoice()
+
+inv.read()
